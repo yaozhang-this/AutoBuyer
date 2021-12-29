4fd711622e857af08e66f2cfa17b553c7293cc21
Yao Zhang <yz750@georgetown.edu>
2021-12-29 18:01:20 -0500

    Add walmart and target search, add headless and finer-tiered configuration for each run

--- iptest.py
+++ iptest.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,6 +12,7 @@ from urllib.parse import urlparse
 from selenium.webdriver.support.wait import WebDriverWait
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
 geckodriver_autoinstaller.install()
 
 
@@ -54,7 +56,7 @@ def refreshIPTable():
 #  'Https': 'yes',
 #  'Last Checked': '8 secs ago'}
 
-def ipCheck(filepath, visit_limits=2000, notification=True, headless=False,):
+def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, headless=False, addtocart=True, checkout=False):
 	# proxies = refreshIPTable()
 	index = 0
 	# index = random.randint(0, len(proxies) - 1)
@@ -67,83 +69,105 @@ def ipCheck(filepath, visit_limits=2000, notification=True, headless=False,):
 	# }
 	textFile = open(filepath, "r")
 	lines = textFile.readlines()
+	textFile.close()
 	print(lines)
-	drivers = []
 	foundButtons = []
+	foundDrivers = []
+	urls = []
 	for url in lines:
 		if url == '\n':
 			continue
+		urls.append(url)
 		# profile = webdriver.FirefoxProfile(os.path.expanduser("/Volumes/Rocket-XTRM/Users/seseme/Library/Application\ Support/Firefox/Profiles/u5e1vmwr.default-release"))
 		# d = webdriver.Firefox(profile)
-		d = webdriver.Firefox()
-		d.get(url)
-		drivers.append(d)
-		domain = urlparse(url).netloc
-		if domain == "www.bestbuy.com":
-			addToCartButton = addButton = d.find_element_by_class_name("add-to-cart-button")
-			if "c-button-disabled" not in addToCartButton.get_attribute("class"):
-				addToCartButton.click()
-				title = d.find_element_by_tag_name("h1").get_attribute("innerText")
-				price = d.find_element_by_class_name("priceView-hero-price").find_element_by_xpath(
-					".//span").get_attribute("innerText")
-				message = f"{title} {price} added to cart for Bestbuy at {datetime.datetime.now().time()}. Quicklink: {url}"
-				if notification: twi.send_simple_sms(message)
-				print(message)
-				foundButtons.append(True)
-			else:
-				foundButtons.append(False)
-		if domain == "www.gamestop.com":
-			addToCartButton = addButton = d.find_element_by_id("add-to-cart")
-			text = addToCartButton.get_attribute("innerText")
-			if "Not Available" not in text and "Unavailable" not in text:
-				title = d.find_element_by_class_name("product-name").get_attribute("innerText")
-				message = f"{title} is on stock for Gamestop at {datetime.datetime.now().time()}. Quicklink: {url}"
-				if notification: twi.send_simple_sms(message)
-				print(addToCartButton.get_attribute("innerText"))
-				print(message)
-
-				foundButtons.append(True)
-			else:
-				foundButtons.append(False)
+		foundButtons.append(False)
+		foundDrivers.append(None)
 	while True:
+		#reference before exception
+		d = None
 		try:
 			for i, b in enumerate(foundButtons):
 				if b is False:
-					drivers[i].refresh()
-					url = drivers[i].current_url
+					url = urls[i]
 					domain = urlparse(url).netloc
+					fireFoxOptions = webdriver.FirefoxOptions()
+					if headless: fireFoxOptions.headless = True
+					fireFoxOptions.set_preference("geo.enabled", True)
+					fireFoxOptions.set_preference("geo.prompt.testing", True)
+					fireFoxOptions.set_preference("geo.prompt.testing.allow", True)
+					fireFoxOptions.set_preference('geo.provider.network.url',
+											  'data:application/json,{"location": {"lat": 38.918910, "lng": -77.220010}, "accuracy": 100.0}')
+					d = webdriver.Firefox(options=fireFoxOptions)
+					d.get(url)
+					action = "is on stock"
 					if domain == "www.bestbuy.com":
-						addToCartButton = addButton = drivers[i].find_element_by_class_name("add-to-cart-button")
+						addToCartButton = addButton = d.find_element_by_class_name("add-to-cart-button")
 						if "c-button-disabled" not in addToCartButton.get_attribute("class"):
-							addToCartButton.click()
-							title = drivers[i].find_element_by_tag_name("h1").get_attribute("innerText")
-							price = drivers[i].find_element_by_class_name("priceView-hero-price").find_element_by_xpath(
+							if addtocart:
+								addToCartButton.click()
+								action = "added to cart"
+							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
+							price = d.find_element_by_class_name("priceView-hero-price").find_element_by_xpath(
 								".//span").get_attribute("innerText")
-							message = f"{title} {price} added to cart for Bestbuy at {datetime.datetime.now().time()}. Quicklink: {url}"
+							message = f"{title} {price} {action} for Bestbuy at {datetime.datetime.now().time()}. Quicklink: {url}"
 							if notification: twi.send_simple_sms(message)
 							print(message)
 							foundButtons[i] = True
-					if domain == "www.gamestop.com":
-						addToCartButton = addButton = drivers[i].find_element_by_id("add-to-cart")
+					elif domain == "www.gamestop.com":
+						addToCartButton = addButton = d.find_element_by_id("add-to-cart")
 						text = addToCartButton.get_attribute("innerText")
 						if "Not Available" not in text and "Unavailable" not in text:
-							title = drivers[i].find_element_by_class_name("product-name").get_attribute("innerText")
-							message = f"{title} is on stock for Gamestop at {datetime.datetime.now().time()}. Quicklink: {url}"
+							title = d.find_element_by_class_name("product-name").get_attribute("innerText")
+							message = f"{title} {action} for Gamestop at {datetime.datetime.now().time()}. Quicklink: {url}"
 							if notification: twi.send_simple_sms(message)
-							print(text, "Not Available")
-							print(message)
 							foundButtons[i] = True
-					time.sleep(2)
-					visit_limits -= 1
-					if visit_limits == 0:
-						for i, f in enumerate(foundButtons):
-							if not f:
-								url = lines[i]
-								drivers[i].close()
-								drivers[i].quit()
-								drivers[i] = webdriver.Firefox()
-								drivers[i].get(url)
-						break
+					elif domain == "www.walmart.com":
+						#need to modify webdriver
+						try:
+							addToCartButton = d.find_element_by_xpath("//*[@id=\"__next\"]/div[1]/div/div/div/div/section/main/div/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div[3]/button")
+							if addtocart:
+								addToCartButton.click()
+								action = "added to cart"
+							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
+							message = f"{title} {action} for Walmart at {datetime.datetime.now().time()}. Quicklink: {url}"
+							if notification: twi.send_simple_sms(message)
+							foundButtons[i] = True
+						except Exception as e:
+							# not available/out of stock
+							pass
+
+					elif domain == "www.target.com":
+						try:
+							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
+							message = f"{title} {action} for Target at {datetime.datetime.now().time()}. Quicklink: {url}"
+							print(message)
+							if notification: twi.send_simple_sms(message)
+							foundButtons[i] = True
+
+						except Exception as e:
+							#not available
+							pass
+					if foundButtons[i]:
+						if addtocart: foundDrivers[i] = d
+						else:
+							time.sleep(1)
+							d.close()
+							d.quit()
+					else:
+						time.sleep(1)
+						d.close()
+						d.quit()
+
+					if haslimit:
+						visits_limit -= 1
+						if visits_limit == 0:
+							d.close()
+							d.quit()
+							message = f"Configured visits limit of {visits_limit} reached, terminating crawler"
+							if notification: twi.send_simple_sms(message)
+							print(message)
+							sys.exit()
+
 
 
 		except Exception as e:
@@ -151,18 +175,14 @@ def ipCheck(filepath, visit_limits=2000, notification=True, headless=False,):
 			# delete bad proxy from list
 			# could optimize
 			# print(f"Removed {proxies.pop(index)} from list")
-			for i, f in enumerate(foundButtons):
-				if not f:
-					url = lines[i]
-					drivers[i].close()
-					drivers[i].quit()
-					drivers[i] = webdriver.Firefox()
-					drivers[i].get(url)
+			# close and retry
+			d.close()
+			d.quit()
 
 
 def main():
 	# every 10 minutes, refresh ip table and ping each of them
-	ipCheck('test.txt')
+	ipCheck('test.txt', headless=True, addtocart=False)
 
 
 if __name__ == '__main__':
