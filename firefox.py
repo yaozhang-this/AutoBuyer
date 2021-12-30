de8f7a0901529f94dd872f26a8fe4c2333658a7c
Yao Zhang <yz750@georgetown.edu>
2021-12-29 21:29:01 -0500

    Revise formatting.

--- firefox.py
+++ firefox.py
@@ -1,22 +1,31 @@
+import datetime
+import logging
 import sys
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import geckodriver_autoinstaller
-import random, datetime, os
-import sendtwilio as twi
+import warnings  # install geckodriver via pip
 from urllib.parse import urlparse
 
-# install geckodriver via pip
-from selenium.webdriver.support.wait import WebDriverWait
-import warnings
+import geckodriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import send_twilio as twi
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 geckodriver_autoinstaller.install()
 
 
-def refreshIPTable():
+def refresh_ip_table():
+
+	"""
+	refresh_ip_table crawls free proxies from sslproxies.org
+
+	:return: proxies: list of dictionaries of ssl proxies
+	"""
+
 	with webdriver.Firefox() as driver:
 		driver.get("https://sslproxies.org/")
 		table = driver.find_element(By.TAG_NAME, 'table')
@@ -43,24 +52,38 @@ def refreshIPTable():
 						pass
 				else:
 					proxies.append(proxy_data)
+		# Format of the dictionary
+		# {'IP Address': '200.85.169.18',
+		#  'Port': '47548',
+		#  'Code': 'NI',
+		#  'Country': 'Nicaragua',
+		#  'Anonymity': 'elite proxy',
+		#  'Google': 'no',
+		#  'Https': 'yes',
+		#  'Last Checked': '8 secs ago'}
 		return proxies
 
 
-# Format of the dictionary
-# {'IP Address': '200.85.169.18',
-#  'Port': '47548',
-#  'Code': 'NI',
-#  'Country': 'Nicaragua',
-#  'Anonymity': 'elite proxy',
-#  'Google': 'no',
-#  'Https': 'yes',
-#  'Last Checked': '8 secs ago'}
+def crawl(filepath, has_limit=False, visits_limit=2000, notification=True, headless=False, add_to_cart=True,
+		  checkout=False):
+	"""
+	crawl does periodic crawling of selected sites from input file, has capabilities to perform actions case-by-case
 
-def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, headless=False, addtocart=True, checkout=False):
+	:param filepath: absolute path of input file that contains urls
+    :param has_limit: flag for limiting access frequency
+    :param visits_limit: quantity of access limit
+    :param notification: flag for remote notification of in-stock alert
+    :param headless: flag for headless execution of webdriver
+    :param add_to_cart: flag for default action performed
+    :param checkout: flag for default action performed
+
+	:return None
+	"""
 	# proxies = refreshIPTable()
-	index = 0
+	# index = 0
 	# index = random.randint(0, len(proxies) - 1)
 	# PROXY = proxies[index]["IP Address"] + ":" + proxies[index]["Port"]
+	# "IPv4:PORT"
 	# PROXY = "149.19.224.15:3128"
 	# webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
 	# 	"httpProxy": PROXY,
@@ -70,7 +93,7 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 	textFile = open(filepath, "r")
 	lines = textFile.readlines()
 	textFile.close()
-	print(lines)
+	logging.info(f"list of url to crawl: {lines}")
 	foundButtons = []
 	foundDrivers = []
 	urls = []
@@ -78,12 +101,10 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 		if url == '\n':
 			continue
 		urls.append(url)
-		# profile = webdriver.FirefoxProfile(os.path.expanduser("/Volumes/Rocket-XTRM/Users/seseme/Library/Application\ Support/Firefox/Profiles/u5e1vmwr.default-release"))
-		# d = webdriver.Firefox(profile)
 		foundButtons.append(False)
 		foundDrivers.append(None)
 	while True:
-		#reference before exception
+		# reference before exception
 		d = None
 		try:
 			for i, b in enumerate(foundButtons):
@@ -96,14 +117,15 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 					fireFoxOptions.set_preference("geo.prompt.testing", True)
 					fireFoxOptions.set_preference("geo.prompt.testing.allow", True)
 					fireFoxOptions.set_preference('geo.provider.network.url',
-											  'data:application/json,{"location": {"lat": 38.918910, "lng": -77.220010}, "accuracy": 100.0}')
+												  'data:application/json,{"location": {"lat": 38.918910, "lng": -77.220010}, "accuracy": 100.0}')
 					d = webdriver.Firefox(options=fireFoxOptions)
 					d.get(url)
 					action = "is on stock"
+
 					if domain == "www.bestbuy.com":
-						addToCartButton = addButton = d.find_element_by_class_name("add-to-cart-button")
+						addToCartButton = d.find_element_by_class_name("add-to-cart-button")
 						if "c-button-disabled" not in addToCartButton.get_attribute("class"):
-							if addtocart:
+							if add_to_cart:
 								addToCartButton.click()
 								action = "added to cart"
 							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
@@ -111,28 +133,33 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 								".//span").get_attribute("innerText")
 							message = f"{title} {price} {action} for Bestbuy at {datetime.datetime.now().time()}. Quicklink: {url}"
 							if notification: twi.send_simple_sms(message)
-							print(message)
+							logging.info(message)
 							foundButtons[i] = True
+
 					elif domain == "www.gamestop.com":
-						addToCartButton = addButton = d.find_element_by_id("add-to-cart")
+						addToCartButton = d.find_element_by_id("add-to-cart")
 						text = addToCartButton.get_attribute("innerText")
 						if "Not Available" not in text and "Unavailable" not in text:
 							title = d.find_element_by_class_name("product-name").get_attribute("innerText")
 							message = f"{title} {action} for Gamestop at {datetime.datetime.now().time()}. Quicklink: {url}"
 							if notification: twi.send_simple_sms(message)
+							logging.info(message)
 							foundButtons[i] = True
+
 					elif domain == "www.walmart.com":
-						#need to modify webdriver
+						# need to modify webdriver
 						try:
-							addToCartButton = d.find_element_by_xpath("//*[@id=\"__next\"]/div[1]/div/div/div/div/section/main/div/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div[3]/button")
-							if addtocart:
+							addToCartButton = d.find_element_by_xpath(
+								"//*[@id=\"__next\"]/div[1]/div/div/div/div/section/main/div/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div/div/div[3]/button")
+							if add_to_cart:
 								addToCartButton.click()
 								action = "added to cart"
 							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
 							message = f"{title} {action} for Walmart at {datetime.datetime.now().time()}. Quicklink: {url}"
 							if notification: twi.send_simple_sms(message)
+							logging.info(message)
 							foundButtons[i] = True
-						except Exception as e:
+						except Exception:
 							# not available/out of stock
 							pass
 
@@ -140,15 +167,16 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 						try:
 							title = d.find_element_by_tag_name("h1").get_attribute("innerText")
 							message = f"{title} {action} for Target at {datetime.datetime.now().time()}. Quicklink: {url}"
-							print(message)
+							logging.info(message)
 							if notification: twi.send_simple_sms(message)
 							foundButtons[i] = True
 
 						except Exception as e:
-							#not available
+							# not available
 							pass
 					if foundButtons[i]:
-						if addtocart: foundDrivers[i] = d
+						if add_to_cart:
+							foundDrivers[i] = d
 						else:
 							time.sleep(1)
 							d.close()
@@ -158,20 +186,21 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 						d.close()
 						d.quit()
 
-					if haslimit:
+					if has_limit:
 						visits_limit -= 1
 						if visits_limit == 0:
 							d.close()
 							d.quit()
 							message = f"Configured visits limit of {visits_limit} reached, terminating crawler"
 							if notification: twi.send_simple_sms(message)
-							print(message)
+							logging.info(message)
 							sys.exit()
 
-
+		except KeyboardInterrupt:
+			sys.exit()
 
 		except Exception as e:
-			print(f"{e} occured at {datetime.datetime.now().time()}")
+			logging.error(f"{e} occured at {datetime.datetime.now().time()}")
 			# delete bad proxy from list
 			# could optimize
 			# print(f"Removed {proxies.pop(index)} from list")
@@ -181,8 +210,7 @@ def ipCheck(filepath, haslimit=False, visits_limit=2000, notification=True, head
 
 
 def main():
-	# every 10 minutes, refresh ip table and ping each of them
-	ipCheck('test.txt', headless=True, addtocart=False)
+	crawl('ps5_source.txt', headless=True, add_to_cart=False)
 
 
 if __name__ == '__main__':
